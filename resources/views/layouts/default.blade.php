fcf104ed3a84421370cb9759231b8d1d3c12da08
lifei <zzxxe@163.com>
2017-11-15 06:37:04 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Sample') - Laravel 入门教程</title>
-</head>
-<body>
-   @yield('content')
-</body>
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+      </div>
+    </div>    
+  </body>
 </html>
